d5154185855d1fbec75ec145595be2a50c97d1fe
lauanS <lauan.s.souza@gmail.com>
2024-04-30 03:19:42 -0300

    :feat: Add authentification routes

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\Api\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
 
 Route::get('/tasks', [TaskController::class, 'index']);
 Route::post('tasks', [TaskController::class, 'store']);
