ac7e1bb9114c4f51974ecf40e9004cb830e63a00
lauanS <lauan.s.souza@gmail.com>
2024-04-30 12:32:13 -0300

    chore: Improve Task Migration

--- api/database/migrations/2024_04_30_023243_create_tasks_table.php
+++ api/database/migrations/2024_04_30_023243_create_tasks_table.php
@@ -14,8 +14,9 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description');
-            $table->string('user_id');
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
