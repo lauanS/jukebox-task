014770f0292a0b41ca2ecf43008f54cd2ccdb0ac
lauanS <lauan.s.souza@gmail.com>
2024-05-01 12:12:27 -0300

    feat: add login page

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import TaskView from "../views/TaskView.vue";
+import TaskView from "@/views/Task/TaskPage.vue";
+import LoginView from "@/views/Login/LoginPage.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -8,13 +9,9 @@ const routes: Array<RouteRecordRaw> = [
     component: TaskView,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/login",
+    name: "login",
+    component: LoginView,
   },
 ];
 
---
+++ web/src/views/Login/LoginPage.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center">
+    <div class="max-w-md w-full p-6 bg-slate-900 rounded-lg shadow-md">
+      <h2 class="text-2xl font-bold py-6">Login</h2>
+      <form @submit.prevent="login" class="space-y-4">
+        <div>
+          <label for="username" class="block font-medium">
+            Nome de usuário:
+          </label>
+          <input
+            v-model="username"
+            type="text"
+            id="username"
+            class="mt-1 block w-full rounded text-gray-900"
+          />
+        </div>
+        <div>
+          <label for="password" class="block font-medium">Senha:</label>
+          <input
+            v-model="password"
+            type="password"
+            id="password"
+            class="mt-1 block w-full rounded text-gray-900"
+          />
+        </div>
+        <button
+          type="submit"
+          class="w-full bg-blue-500 hover:bg-blue-600 font-bold py-2 px-4 rounded"
+        >
+          Entrar
+        </button>
+        <span class="block">Já possui conta? <a>Cadastre-se</a></span>
+        <span class="block">Esqueceu sua senha? <a>Redefina sua senha</a></span>
+        <div v-if="error" class="text-red-500">{{ error }}</div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+
+const username = ref("");
+const password = ref("");
+const error = ref<string | null>(null);
+
+const login = () => {
+  console.log("LoginPage::login");
+  console.log(
+    store.dispatch("auth/login", {
+      email: "lauan.s.souza@gmail.com",
+      password: "Senha123",
+    })
+  );
+};
+</script>
