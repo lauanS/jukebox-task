36644d7adc52d558a13157784c2998323fd3ab77
lauanS <lauan.s.souza@gmail.com>
2024-05-01 22:26:20 -0300

    refactor: Remove unnecessary csrf() calls

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -18,21 +18,17 @@ export type Task = {
 export const csrf = () => client.get("/sanctum/csrf-cookie");
 
 export const listTasksService = async () => {
-  await csrf();
   return await client.get("/api/tasks");
 };
 
 export const createTaskService = async (payload: Task) => {
-  await csrf();
   return await client.post("/api/tasks", { ...payload, user_id: 1 });
 };
 
 export const updateTaskService = async (id: number, payload: Task) => {
-  await csrf();
   return await client.put(`/api/tasks/${id}`, { ...payload, user_id: 1 });
 };
 
 export const deleteTaskService = async (id: number) => {
-  await csrf();
   return await client.delete(`/api/tasks/${id}`);
 };
